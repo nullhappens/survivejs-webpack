52c42f439d0d9a4f038477634996a95472b1b6ed
Diego Pineda <dpineda@nullhappens.com>
2017-06-21 22:04:04 -0400

    added library compilation example

---
+++ webpack.lib.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const parts = require('./webpack.parts');
+const PATHS = {
+  lib: path.join(__dirname, 'lib'),
+  build: path.join(__dirname, 'dist')
+};
+
+const commonConfig = merge([
+  {
+    entry: {
+      lib: PATHS.lib
+    },
+    output: {
+      path: PATHS.build,
+      library: 'Demo',
+      libraryTarget: 'var'
+    }
+  },
+  parts.attachRevision(),
+  parts.generateSourceMaps({ type: 'source-map' }),
+  parts.loadJs({ include: PATHS.lib })
+]);
+
+const libraryConfig = merge([
+  commonConfig,
+  {
+    output: {
+      filename: '[name].js'
+    }
+  },
+  parts.clean(PATHS.build),
+  parts.lintJs({ include: PATHS.lib })
+]);
+
+const libraryMinConfig = merge([
+  commonConfig,
+  {
+    output: {
+      filename: '[name].min.js'
+    }
+  },
+  parts.minifyJavascript()
+]);
+
+module.exports = [
+  libraryConfig,
+  libraryMinConfig
+];
